72570c9256f9865d4a46a6c80ba02575735d2506
wangxiaomo <wxm4ever@gmail.com>
2014-12-04 10:01:57 +0800

    使用提示

--- app/statics/js/app.js
+++ app/statics/js/app.js
@@ -1,4 +1,4 @@
-angular.module('ngFancy', ['ngRoute'])
+angular.module('ngFancy', ['ngRoute', 'ui.bootstrap'])
   .config(['$routeProvider', function($routeProvider){
     $routeProvider
       .when('/', {
--- app/statics/js/controllers/main.js
+++ app/statics/js/controllers/main.js
@@ -1,4 +1,26 @@
 angular.module('ngFancy')
-  .controller('MainCtrl', ['$scope', function($scope) {
+  .controller('MainCtrl', ['$scope', '$modal', function($scope, $modal) {
 
+    $scope.showModal = function () {
+      $('#overlay').show();
+      var modalInstance = $modal.open({
+        templateUrl: 'modal-alert',
+        controller: 'MainModalCtrl',
+        backdrop: false,
+        keyboard: false,
+        size: 'lg',
+      });
+    };
+
+    $scope.go = function () {
+      $scope.showModal();
+    };
+  }]);
+
+angular.module('ngFancy')
+  .controller('MainModalCtrl', ['$scope', '$modalInstance', function($scope, $modalInstance) {
+    $scope.dismiss = function () {
+      $modalInstance.dismiss('cancel');
+      $('#overlay').hide();
+    };
   }]);
