82cdf293bbcfdb39b876d653defd755ded228724
wangxiaomo <wxm4ever@gmail.com>
2015-01-09 13:04:34 +0800

    gulp js map

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var APP_ROOT = './app/',
 var gulp = require('gulp'),
     usemin = require('gulp-usemin'),
     compass = require('gulp-compass'),
+    rename = require('gulp-rename'),
     rev = require('gulp-rev'),
     del = require('del');
 
@@ -17,11 +18,20 @@ gulp.task('compass', function() {
     .pipe(compass({
       config_file: 'config.rb',
       css: APP_ROOT + 'static/css',
-      scss: APP_ROOT + 'static/scss'
+      sass: APP_ROOT + 'static/scss'
     }));
 });
 
 gulp.task('usemin', ['clean', 'compass'], function() {
+  gulp.src(APP_ROOT + 'static/images/**')
+    .pipe(gulp.dest(DIST_ROOT + 'static/images'));
+
+  gulp.src(['./bower_components/**/*.map'])
+    .pipe(rename({
+      dirname: ''
+    }))
+    .pipe(gulp.dest(DIST_ROOT + 'static/js'));
+
   return gulp.src(APP_ROOT + '**/*.html')
     .pipe(usemin({
       css: ['concat', rev()],
@@ -29,3 +39,6 @@ gulp.task('usemin', ['clean', 'compass'], function() {
     }))
     .pipe(gulp.dest(DIST_ROOT));
 });
+
+gulp.task('build', ['clean', 'usemin']);
+gulp.task('default', ['build']);
