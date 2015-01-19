25236d4a8bdd00888e695d12d4f6fce93064a194
wangxiaomo <wxm4ever@gmail.com>
2015-01-19 14:55:26 +0800

    0119

--- gulpfile.js
+++ gulpfile.js
@@ -2,21 +2,23 @@ var APP_ROOT = './app/',
     DIST_ROOT = './dist/';
 
 var gulp = require('gulp'),
+    gutil = require('gulp-util'),
     usemin = require('gulp-usemin'),
     compass = require('gulp-compass'),
-    imagemin = require('gulp-imagemin'),
-    livereload = require('gulp-livereload'),
-    serveStatic = require('serve-static'),
+    coffee = require('gulp-coffee'),
+    rename = require('gulp-rename'),
     rev = require('gulp-rev'),
     del = require('del');
 
+
 gulp.task('clean', function(cb) {
-  return del(['app/statics/css/*', 'dist'], cb);
+  return del(['app/statics/css/*', DIST_ROOT], cb);
 });
 
-gulp.task('webfont', function() {
-  return gulp.src(APP_ROOT + 'statics/fonts/*')
-    .pipe(gulp.dest(DIST_ROOT + 'statics/fonts'));
+gulp.task('coffee', function() {
+  return gulp.src(APP_ROOT + 'statics/coffee/**/*.coffee')
+    .pipe(coffee({bare: true}).on('error', gutil.log))
+    .pipe(gulp.dest(APP_ROOT + 'statics/js/'));
 });
 
 gulp.task('compass', function() {
@@ -28,7 +30,19 @@ gulp.task('compass', function() {
     }));
 });
 
-gulp.task('usemin', ['clean', 'webfont', 'compass', 'imagemin'], function() {
+gulp.task('usemin', ['clean', 'compass', 'coffee'], function() {
+  gulp.src(APP_ROOT + 'statics/images/**')
+    .pipe(gulp.dest(DIST_ROOT + 'statics/images'));
+
+  gulp.src(APP_ROOT + 'statics/fonts/**')
+    .pipe(gulp.dest(DIST_ROOT + 'statics/fonts'));
+
+  gulp.src(['./bower_components/**/*.map'])
+    .pipe(rename({
+      dirname: ''
+    }))
+    .pipe(gulp.dest(DIST_ROOT + 'statics/js'));
+
   return gulp.src(APP_ROOT + '**/*.html')
     .pipe(usemin({
       css: ['concat', rev()],
@@ -37,28 +51,5 @@ gulp.task('usemin', ['clean', 'webfont', 'compass', 'imagemin'], function() {
     .pipe(gulp.dest(DIST_ROOT));
 });
 
-gulp.task('sync', ['usemin'], function(){
-  return gulp.src(APP_ROOT + '**')
-    .pipe(livereload());
-});
-
-gulp.task('imagemin', function() {
-  return gulp.src(APP_ROOT + 'statics/images/*')
-    .pipe(imagemin({optimizationLevel: 5}))
-    .pipe(gulp.dest(DIST_ROOT + 'statics/images'))
-});
-
-gulp.task('server', function(next) {
-  var connect = require('connect'),
-      server = connect();
-  server.use(serveStatic('.')).listen(8000, next);
-});
-
-gulp.task('watch', ['server'], function() {
-  livereload.listen()
-  gulp.watch(APP_ROOT + '**/*.html', ['usemin', 'sync']);
-  gulp.watch(APP_ROOT + 'statics/scss/*.scss', ['compass', 'usemin', 'sync']);
-  gulp.watch(APP_ROOT + 'statics/images/*', ['imagemin']);
-});
-
-gulp.task('default', ['clean', 'usemin']);
+gulp.task('build', ['clean', 'usemin']);
+gulp.task('default', ['build']);
