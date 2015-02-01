c580d45ea203480d698b9a97292971ed991fa1bc
wangxiaomo <wxm4ever@gmail.com>
2015-02-01 15:36:07 +0800

    update done page

--- app/statics/js/controllers/done.js
+++ app/statics/js/controllers/done.js
@@ -1,7 +1,17 @@
 angular.module('ngFancy')
   .controller('DoneCtrl', ['$scope', '$localStorage', '$location', function($scope, $localStorage, $location) {
     
-    $scope.event = $localStorage.events[$localStorage.lastEventID];
+    try {
+      $scope.event = $localStorage.events[$localStorage.lastEventID];
+    }catch (error) {
+      $location.path('/');
+    }
+
+    if($scope.event.role) {
+      $scope.center = FANCY_CENTERS[$scope.event.role];
+    }else{
+      $scope.fallback = true;
+    }
 
     $scope.goHome = function () {
       delete $localStorage.lastEventID;
---
+++ app/statics/js/source.js
@@ -0,0 +1,7 @@
+window.FANCY_CENTERS = {
+  '小店区快处中心': {
+    name: '小店区快处中心',
+    tel: '0351-1234567',
+    addr: 'xxxx'
+  },
+};
